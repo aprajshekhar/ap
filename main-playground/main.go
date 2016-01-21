b7ea4cf7cef29271d4c63a22ed48654dbb5fdc91
A.P. Rajshekhar <randalap@redhat.com>
2016-01-21 16:40:03 +0530

    added get repository details functionality

--- main-playground/main.go
+++ main-playground/main.go
@@ -7,12 +7,26 @@ import (
 )
 
 func main() {
-	pc := pulp.NewClient("url", "", "", "user", "pwd")
-	//var rep pulp.Repository
-	rep, _ := pc.GetRepositories()
+	pc := pulp.NewClient("url", "", "", "user", "passwd")
+	var rep pulp.Repositories
+	rep, _ = pc.ListRepositories()
 	err := pc.Authenticate()
 	fmt.Println("error in auth: ", err)
-	fmt.Println(pc.Cert.PkiKey)
+	fmt.Println("received key:", pc.Cert.PkiKey)
 	fmt.Println("==================================================================")
-	fmt.Println(rep)
+	for _, repo := range rep {
+		fmt.Println("url :", repo.URL)
+		fmt.Println("display name:", repo.Display)
+		fmt.Println("repo id:", repo.RepoId)
+		fmt.Println("description: ", repo.Description)
+		fmt.Println("------------------------------")
+	}
+
+	fmt.Println("==================================================================")
+	fmt.Println("retrieving details of repository: redhat-rhel7-docker-hello-world ")
+	var repository pulp.RepositoryDetails
+	repository, _ = pc.GetRepository("redhat-rhel7-docker-hello-world")
+	fmt.Println("url: ", repository.URL)
+	fmt.Println("display name: ", repository.Display)
+
 }
