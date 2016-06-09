f888e964f4980041446716d2585c0e096c5946c6
A.P. Rajshekhar <randalap@redhat.com>
2016-06-09 17:39:24 +0530

    updated for put, post and delete functionality

--- netstorage/netstorage.go
+++ netstorage/netstorage.go
@@ -2,14 +2,14 @@
 package netstorage
 
 import (
-	"golang.org/x/text/encoding/charmap"
-	"golang.org/x/text/transform"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/xml"
 	"errors"
 	"fmt"
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/transform"
 	"io"
 	"math/rand"
 	"net"
@@ -20,84 +20,72 @@ import (
 	"time"
 )
 
-const version = "1"
-
-type httpError struct {
-	error
-	code int
+// A NetstorageClient uploads to Akamai Netstorage using HTTP:
+// https://control.akamai.com/dl/customers/NS/NS_http_api_FS.pdf
+// (login required)
+type NetstorageClient struct {
+	Host              string
+	Folder            string
+	Prefix            string
+	BaseURL           string
+	NetstorageKeyName string
+	NetstorageSecret  string
 }
 
-func NewHTTPError(resp *http.Response) *httpError {
-	code := resp.StatusCode
-	return &httpError{
-		error: errors.New(http.StatusText(code)),
-		code:  code,
+func (client *NetstorageClient) SetPrefix(key string) {
+	client.Prefix = key
+}
+
+func (client *NetstorageClient) URLFor(p string) string {
+	return fmt.Sprintf("%s/%s.json", client.BaseURL, path.Join(client.Prefix, p))
+}
+
+func (client *NetstorageClient) auth(r *http.Request, id string, filename string, unixTime int64) {
+	action := "version=1&action=upload"
+	r.Header.Set("X-Akamai-ACS-Action", action)
+	authData := fmt.Sprintf("5, 0.0.0.0, 0.0.0.0, %d, %s, %s", unixTime, id, client.NetstorageKeyName)
+	r.Header.Set("X-Akamai-ACS-Auth-Data", authData)
+	hash := hmac.New(sha256.New, []byte(client.NetstorageSecret))
+	fmt.Fprintf(hash, "%s/%s\nx-akamai-acs-action:%s\n", authData, filename, action)
+	r.Header.Set("X-Akamai-ACS-Auth-Sign", base64.StdEncoding.EncodeToString(hash.Sum(nil)))
+}
+
+func (client *NetstorageClient) PutReader(key string, r io.Reader, contentType string) error {
+	filename := path.Join(client.Folder, client.Prefix, key)
+	req, err := http.NewRequest("PUT", fmt.Sprintf("http://%s/%s", client.Host, filename), r)
+	if err != nil {
+		return err
 	}
-}
-
-func NewHTTPErrorWithText(resp *http.Response, txt string) *httpError {
-	code := resp.StatusCode
-	return &httpError{
-		error: errors.New(http.StatusText(code) + " - " + txt),
-		code:  code,
+	client.auth(req, filename, filename, time.Now().Unix())
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
 	}
-}
-
-// Api instances are safe for concurrent use by multiple goroutines
-type NetStorage struct {
-	client  	*http.Client
-	KeyName 	string
-	Secret  	string
-	HostName	string
-	UseSSL		bool
-}
-
-func NewNetStorageDefault(keyName, secret, hostName string) NetStorage {
-	
-	return NetStorage{keyName, secret, hostName, false}
-}
-
-func NewNetStorage(keyName, secret, hostName string, useSSL bool){
-	client := &http.Client{}
-	return Api{client, keyName, secret, hostName, useSSL}
-}
-
-func (netStorage NetStorage)SetKeyName(keyName string){
-	netStorage.KeyName = keyName
-}
-
-func (netStorage NetStorage)SetSecret(secret string){
-	netStorage.Secret = secret
-}
-
-func (netStorage NetStorage)SetHostName(hostName string){
-	netStorage.HostName = hostName
-}
-
-func (netStorage NetStorage)SetUseSSL(useSSL bool){
-	netStorage.UseSSL = useSSL
-}
-
-func (netStorage NetStorage)NetStorageUrl(path string) string{
-	scheme := "http"
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		dump, _ := httputil.DumpResponse(resp, true)
+		return fmt.Errorf("unexpected response code %d when uploading %s. Here's a dump of the response:\n%s", resp.StatusCode, filename, string(dump))
 	}
-	if netStorage.UseSSL {
-		scheme = "https"
+	log.Printf("output: put %s", filename)
+	return nil
+}
+
+func (client *NetstorageClient) Delete(key string) error {
+	filename := path.Join(client.Folder, client.Prefix, key)
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("http://%s/%s", client.Host, filename), nil)
+	if err != nil {
+		return err
 	}
-
-	host := fmt.Sprintf("%s.%s", netStorage.HostName, path)
-
-	netStorageUrl := &url.URL{
-		Scheme: scheme,
-		Host:   host}
-	return netStorageUrl.String()
+	client.auth(req, filename, filename, time.Now().Unix())
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		dump, _ := httputil.DumpResponse(resp, true)
+		return fmt.Errorf("unexpected response code %d when uploading %s. Here's a dump of the response:\n%s", resp.StatusCode, filename, string(dump))
+	}
+	log.Printf("output: delete %s", filename)
+	return nil
 }
-
-func (netStorage NetStorage) auth(req *http.Request, rel_path, action string) {
-	data, signature := api.sign(rel_path, action, -1, -1)
-	req.Header.Add("X-Akamai-ACS-Auth-Data", data)
-	req.Header.Add("X-Akamai-ACS-Auth-Sign", signature)
-}
-
