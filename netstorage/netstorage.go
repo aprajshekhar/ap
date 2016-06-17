f38da0d2651c23d54ac6c94d92213f5ddf889f2a
A.P. Rajshekhar <randalap@redhat.com>
2016-06-17 17:25:22 +0530

    added put, delete and initial dir functionality

--- netstorage/netstorage.go
+++ netstorage/netstorage.go
@@ -32,6 +32,14 @@ type NetstorageClient struct {
 	NetstorageSecret  string
 }
 
+type NSFile struct {
+	Type  string `xml:"type,attr"`
+	Name  string `xml:"name,attr"`
+	Size  int    `xml:"size,attr"`
+	Md5   string `xml:"md5,attr"`
+	Mtime uint32 `xml:"mtime,attr"`
+}
+
 func (client *NetstorageClient) SetPrefix(key string) {
 	client.Prefix = key
 }
@@ -40,14 +48,14 @@ func (client *NetstorageClient) URLFor(p string) string {
 	return fmt.Sprintf("%s/%s.json", client.BaseURL, path.Join(client.Prefix, p))
 }
 
-func (client *NetstorageClient) auth(r *http.Request, id string, filename string, unixTime int64) {
-	action := "version=1&action=upload"
-	r.Header.Set("X-Akamai-ACS-Action", action)
+func (client *NetstorageClient) auth(httpRequest *http.Request, id string, filename string, unixTime int64, actionName string) {
+	action := fmt.Sprint("version=1&action=%s", actionName)
+	httpRequest.Header.Set("X-Akamai-ACS-Action", action)
 	authData := fmt.Sprintf("5, 0.0.0.0, 0.0.0.0, %d, %s, %s", unixTime, id, client.NetstorageKeyName)
-	r.Header.Set("X-Akamai-ACS-Auth-Data", authData)
+	httpRequest.Header.Set("X-Akamai-ACS-Auth-Data", authData)
 	hash := hmac.New(sha256.New, []byte(client.NetstorageSecret))
 	fmt.Fprintf(hash, "%s/%s\nx-akamai-acs-action:%s\n", authData, filename, action)
-	r.Header.Set("X-Akamai-ACS-Auth-Sign", base64.StdEncoding.EncodeToString(hash.Sum(nil)))
+	httpRequest.Header.Set("X-Akamai-ACS-Auth-Sign", base64.StdEncoding.EncodeToString(hash.Sum(nil)))
 }
 
 func (client *NetstorageClient) PutReader(key string, r io.Reader, contentType string) error {
@@ -56,7 +64,7 @@ func (client *NetstorageClient) PutReader(key string, r io.Reader, contentType s
 	if err != nil {
 		return err
 	}
-	client.auth(req, filename, filename, time.Now().Unix())
+	client.auth(req, filename, filename, time.Now().Unix(), "upload")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
@@ -76,7 +84,7 @@ func (client *NetstorageClient) Delete(key string) error {
 	if err != nil {
 		return err
 	}
-	client.auth(req, filename, filename, time.Now().Unix())
+	client.auth(req, filename, filename, time.Now().Unix(), "delete")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
@@ -87,5 +95,25 @@ func (client *NetstorageClient) Delete(key string) error {
 		return fmt.Errorf("unexpected response code %d when uploading %s. Here's a dump of the response:\n%s", resp.StatusCode, filename, string(dump))
 	}
 	log.Printf("output: delete %s", filename)
+	return nil
+}
+
+func (client *NetstorageClient) Dir(key string) error {
+	filename := path.Join(client.Folder, client.Prefix, key)
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s/%s", client.Host, filename), nil)
+	if err != nil {
+		return err
+	}
+	client.auth(req, filename, filename, time.Now().Unix(), "dir")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		dump, _ := httputil.DumpResponse(resp, true)
+		return fmt.Errorf("unexpected response code %d when uploading %s. Here's a dump of the response:\n%s", resp.StatusCode, filename, string(dump))
+	}
+	log.Printf("output: dir %s", filename)
 	return nil
 }
