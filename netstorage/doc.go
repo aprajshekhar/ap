368373b2ad384880e995011bd6c616d4f51063a9
A.P. Rajshekhar <randalap@redhat.com>
2016-07-05 17:49:32 +0530

    added more info in doc.go

--- netstorage/doc.go
+++ netstorage/doc.go
@@ -1,6 +1,7 @@
 // netstorage project doc.go
 
 /*
-netstorage document
+Package netstorage implements pure go client to access Akamai NetStorage upload account of both the types - filestore and objectstore.
+You would require API key name and API key to use this package.
 */
 package netstorage
